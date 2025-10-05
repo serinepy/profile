6f7469b7fe2475b351631d07f0d70f598123e1bf
Serineh <srnprsmn@gmail.com>
2025-10-05 21:40:36 +0330

    add fonts change images and html and css

--- java.js
+++ java.js
@@ -6,3 +6,17 @@ function showMenu() {
       menu.className = "nav";
   }
 }
+
+   var linkContainer = document.getElementById("menu");
+
+    // Get all buttons with class="btn" inside the container
+    var links = linkContainer.getElementsByClassName("header");
+    
+    // Loop through the buttons and add the active class to the current/clicked button
+    for (var i = 0; i < links.length; i++) {
+      links[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+      });
+    } 
